8a06a15dd2aea866ff4ee3eb60f336d3ae4bd3f9
Duc Dung Nguyen <nddunga3@gmail.com>
2018-05-22 17:03:53 +0700

    Initial framework
    Initial framework for the assignment.

---
+++ processData.cpp
@@ -0,0 +1,29 @@
+/*
+ * =========================================================================================
+ * Name        : processData.cpp
+ * Description : Student code for Assignment 2 - Data structures and Algorithms - Spring 2018
+ * =========================================================================================
+ */
+#include "requestLib.h"
+#include "dbLib.h"
+
+/// Initialize and Finalize any global data that you use in the program
+
+bool initVGlobalData(void** pGData) {
+    // TODO: allocate global data if you think it is necessary.
+    /// pGData contains the address of a pointer. You should allocate data for the global data
+    /// and then assign its address to *pGData
+    return true;
+}
+void releaseVGlobalData(void* pGData) {
+    // TODO: release the data if you finish using it
+}
+
+bool processRequest(VRequest& request, L1List<VRecord>& recList, void* pGData) {
+    // TODO: Your code comes here
+
+    /// NOTE: The output of the request will be printed on one line
+    /// end by the end-line '\n' character.
+    return true;
+}
+
---
+++ requestLib.h
@@ -0,0 +1,57 @@
+/*
+ * =========================================================================================
+ * Name        : requestLib.h
+ * Author      : Duc Dung Nguyen
+ * Email       : nddung@hcmut.edu.vn
+ * Copyright   : Faculty of Computer Science and Engineering - Bach Khoa University
+ * Description : Library for Assignment 2 - Data structures and Algorithms - Spring 2018
+ *               This library contains functions used for event management
+ * =========================================================================================
+ */
+
+#ifndef A02_REQUESTLIB_H
+#define A02_REQUESTLIB_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <sstream>
+
+#include "dsaLib.h"
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define REQUEST_CODE_SIZE     16
+using namespace std;
+
+struct VRequest {
+    char        code[REQUEST_CODE_SIZE];
+
+    VRequest() {
+        code[0] = '0';
+        code[1] = 0;
+    }
+    VRequest(char* str) {
+        strncpy(code, str, REQUEST_CODE_SIZE - 1);
+    }
+    VRequest(string& str) {
+        strncpy(code, str.data(), REQUEST_CODE_SIZE - 1);
+    }
+    VRequest(VRequest& a) {
+        memcpy(code, a.code, REQUEST_CODE_SIZE);
+    }
+    VRequest(VRequest&& a) {
+        memcpy(code, a.code, REQUEST_CODE_SIZE);
+    }
+
+    bool operator==(VRequest& b) {
+        return strcmp(code, b.code) == 0;
+    }
+};
+
+void loadRequests(char* fName, L1List<VRequest> &rList);
+
+#endif //A02_REQUESTLIB_H
